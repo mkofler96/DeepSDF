1f6036e05b96cd34892d9a5b68447bf1ca3c4656
Michael Kofler <michael.kofler@tuwien.ac.at>
2024-07-02 13:57:52 +0200

    updated experiments

--- create_ply_files_from_latent.py
+++ create_ply_files_from_latent.py
@@ -11,7 +11,7 @@ import datetime
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-experiment_directory = "./experiments/corner_spheres_only"
+experiment_directory = "./experiments/simple_geom"
 checkpoint = "1000"
 
 decoder = ws.load_trained_model(experiment_directory, checkpoint)
