83fdc09b7a29e392a638e616972ff1b7e16fbd77
Michael Kofler <michael.kofler@tuwien.ac.at>
2024-09-04 10:16:25 +0200

    jupyter updates

--- evaluation_scripts/paper/generate_main_html.py
+++ evaluation_scripts/paper/generate_main_html.py
@@ -21,7 +21,7 @@ index_html_content = """
             margin: 0;
         }}
         .sidebar {{
-            width: 200px;
+            width: 500px;
             background-color: #f4f4f4;
             padding: 15px;
             box-shadow: 2px 0 5px rgba(0,0,0,0.1);
